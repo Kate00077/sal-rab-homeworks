3aa96fb40f378acf258eab1e7bfd8d9c5781cc59
Kate <kt008@mail.ru>
2023-12-03 20:33:14 +0300

    финал1

--- task_final/solution.js
+++ task_final/solution.js
@@ -38,14 +38,8 @@ function sendRequest(name, phone, address, goods, sum) {
         order: {}};
     let countOfGoods = goods.length;
     for (let i = 0; i <= countOfGoods; i += 1) {
-    let goodsData = {
-            title: goods[i].title,
-            count: goods[i].count
-        }; 
-       
-        data.goods.push({title: goods[i].title, count: goods[i].count});
+    data.goods.push({title: goods[i].title, count: goods[i].count});
     };
-    
     data.order.address = "ул. " + address.street + ", дом " + address.house + " , " + address.entrance + " подъезд," + address.floor + " этаж, " + "кв. " + address.flat;
     data.order.sum = sum;
     data.client = name + " " + phone;
